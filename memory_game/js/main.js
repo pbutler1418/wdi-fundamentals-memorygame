3c4793b9a0fac07afd32debc45cb2c51c695b953
Pierce Butler <pbutler1418@gmail.com>
2020-01-02 11:26:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,15 @@
 
 const cards = ["queen", "queen", "king", "king"] ;
 const cardsInPlay = [];
-let cardOne = cards[0]
-cardsInPlay.push("cardOne");
 
-let cardTwo = cards[2]
-cardsInPlay.push("cardTwo")
-console.log(cardsInPlay)
-
-if cardsInPlay[0] === cardsInPlay[1]
-	alert("You found a match!");
+function checkForMatch 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+}
 else
-	alert("sorry, try again.")
+	console.log("sorry, try again.");
+
+
+function flipCard(cardID) {
+	console.log(cards[cardId])
+	cardsInPlay.push(cards[cardId])
