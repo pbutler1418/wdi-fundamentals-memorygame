130da133d5d1c3e34f8105eab873460f3e23d235
Pierce Butler <pbutler1418@gmail.com>
2020-01-02 11:36:48 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-const cards = ["queen", "queen", "king", "king"] ;
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+}
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+}
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+}
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+] ;
 const cardsInPlay = [];
 
 function checkForMatch 
@@ -11,5 +32,5 @@ else
 
 
 function flipCard(cardID) {
-	console.log(cards[cardId])
-	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank)
